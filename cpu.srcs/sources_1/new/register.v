391129257e4c7c09079092b109788ca558dce0a6
Turenar <eclair@turenar.xyz>
2017-12-05 17:54:07 +0900

    remove unused debug code

--- cpu.srcs/sources_1/new/register.v
+++ cpu.srcs/sources_1/new/register.v
@@ -11,24 +11,6 @@ module register(
 	reg [4:0] r1_i;
 	reg [4:0] r2_i;
 	reg [31:0] files [0:31];
-	wire [31:0] r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15;
-
-	// debug
-	assign r1 = files[1];
-	assign r2 = files[2];
-	assign r3 = files[3];
-	assign r4 = files[4];
-	assign r5 = files[5];
-	assign r6 = files[6];
-	assign r7 = files[7];
-	assign r8 = files[8];
-	assign r9 = files[9];
-	assign r10 = files[10];
-	assign r11 = files[11];
-	assign r12 = files[12];
-	assign r13 = files[13];
-	assign r14 = files[14];
-	assign r15 = files[15];
 
 	integer i;
 	always @(posedge clk or posedge rst) begin
