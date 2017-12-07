8bc6e15da934014d4e0ee439c20cacafcada86bf
Turenar <eclair@turenar.xyz>
2017-12-07 17:29:15 +0900

    writeback outs npc

--- cpu.srcs/sources_1/new/writeback.v
+++ cpu.srcs/sources_1/new/writeback.v
@@ -13,6 +13,7 @@ module writeback(
 	input in_mem_enabled,
 	input [31:0] in_mem_addr,
 	input [31:0] in_mem_data,
+	output [31:0] out_npc,
 	output out_set_pc_pulse,
 	output [4:0] out_reg_index,
 	output [31:0] out_reg_map,
@@ -27,6 +28,7 @@ module writeback(
 	reg [`ERRC_BITDEF] Rerrno; assign out_errno = Rerrno;
 	reg Rset_pc_pulse; assign out_set_pc_pulse = Rset_pc_pulse;
 	reg [31:0] Rreg_map; assign out_reg_map = Rreg_map;
+	reg [31:0] Rnpc; assign out_npc = Rnpc;
 	reg [4:0] Rreg_index; assign out_reg_index = Rreg_index;
 	reg [31:0] Rreg_data; assign out_reg_data = Rreg_data;
 	reg Rpc_enabled; assign out_pc_enabled = Rpc_enabled;
@@ -37,11 +39,12 @@ module writeback(
 
 	always @ (posedge clk or posedge rst) begin
 		if(rst) begin
-			Rerrno <= 0; Rreg_map <= 0;
+			Rerrno <= 0; Rnpc <= `PC_ILLEGAL; Rreg_map <= 0;
 			Rreg_index <= 0; Rpc_enabled <= 0; Rmem_enabled <= 0;
 			Rreg_data <= 0; Rpc_addr <= 0; Rmem_addr <= 0; Rmem_data <= 0;
 		end else begin
 			Rerrno <= in_errno;
+			Rnpc <= in_npc;
 			Rreg_map <= in_reg_map;
 			Rreg_index <= in_reg_index;
 			Rreg_data <= in_reg_data;
