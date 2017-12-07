33f6df46294908494f584f9afec2a6660095c16b
Turenar <eclair@turenar.xyz>
2017-12-07 18:18:56 +0900

    clk+0 (direct wb) = next pc, clk+1 (from ic) = next pc+4

--- cpu.srcs/sources_1/new/cpu.v
+++ cpu.srcs/sources_1/new/cpu.v
@@ -189,7 +189,7 @@ module cpu(
 	assign mab_wb_w_enabled = mem_wenabled;
 	assign mab_wb_w_addr = mem_w_addr;
 	assign ic_set_enabled = wb_pc_enabled;
-	assign ic_set_addr = wb_pc_addr;
+	assign ic_set_addr = wb_pc_addr + 4;
 	assign pipeline_flush = wb_set_pc_pulse;
 
 	assign halt = wb_errno != `ERRC_NOERR;
