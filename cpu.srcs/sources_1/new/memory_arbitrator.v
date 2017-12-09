f862beab6abcd2324e7c4ea5e36ba9708ee8b910
Soma Ueda <sora@turenar.xyz>
2017-12-10 01:47:26 +0900

    mab has 4 lock bucket

--- cpu.srcs/sources_1/new/memory_arbitrator.v
+++ cpu.srcs/sources_1/new/memory_arbitrator.v
@@ -8,15 +8,23 @@ module memory_arbitrator (
 	input wb_w_enabled,
 	input [31:0] wb_w_addr);
 
-	reg Rlocked;
+	reg [3:0] Rlocked;
 	reg Rdec_locked_fault; assign dec_locked_fault = Rdec_locked_fault;
+	wire [3:0] Wdec_r_bitmask, Wdec_w_bitmask, Wwb_w_bitmask;
+
+	function [3:0] Fbitmask(input Aenabled, input [31:0] Aaddr);
+		Fbitmask = Aenabled ? (1 << Aaddr[3:2]) : 0;
+	endfunction
+	assign Wdec_r_bitmask = Fbitmask(dec_r_enabled, dec_r_addr);
+	assign Wdec_w_bitmask = Fbitmask(dec_w_enabled, dec_w_addr);
+	assign Wwb_w_bitmask = Fbitmask(wb_w_enabled, wb_w_addr);
 
 	always @ (posedge clk or posedge rst) begin
 		if (rst) begin
 			Rlocked <= 0; Rdec_locked_fault <= 0;
 		end else begin
-			Rlocked <= (Rlocked && ~wb_w_enabled) || dec_w_enabled;
-			Rdec_locked_fault <= (Rlocked && ~wb_w_enabled) && (dec_r_enabled || dec_w_enabled);
+			Rlocked <= (Rlocked & ~Wwb_w_bitmask) | Wdec_w_bitmask;
+			Rdec_locked_fault <= 0 != ((Rlocked & ~Wwb_w_bitmask) & (Wdec_r_bitmask | Wdec_w_bitmask));
 		end
 	end
 endmodule // register_arbitrator
