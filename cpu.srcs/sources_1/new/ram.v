f8778e93d45e4f5be3df89b6ae5920d4cb0f1aa5
Turenar <eclair@turenar.xyz>
2017-12-12 11:53:15 +0900

    resettable

--- cpu.srcs/sources_1/new/ram.v
+++ cpu.srcs/sources_1/new/ram.v
@@ -19,10 +19,12 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-`define MEM_SIZE 65536
+`define MEM_ADDR_WIDTH 13
+`define MEM_MASK_DEF 31:`MEM_ADDR_WIDTH
+`define MEM_SIZE (1<<(`MEM_ADDR_WIDTH-2))
 
 module ram(
-	input clk,
+	input clk, input rst,
 	input we,
 	input [31:0] r1_addr,
 	input [31:0] r1_data,
@@ -36,9 +38,9 @@ module ram(
 	reg [31:0] mem [0:(`MEM_SIZE-1)];
 
 	wire [13:0] Wr1_index, Wr2_index, Ww_index;
-	assign Wr1_index = r1_addr[15:2];
-	assign Wr2_index = r2_addr[15:2];
-	assign Ww_index = w_addr[15:2];
+	assign Wr1_index = r1_addr[`MEM_MASK_DEF] == 0 ? r1_addr[`MEM_ADDR_WIDTH:2] : 0;
+	assign Wr2_index = r2_addr[`MEM_MASK_DEF] == 0 ? r2_addr[`MEM_ADDR_WIDTH:2] : 0;
+	assign Ww_index = w_addr[`MEM_MASK_DEF] == 0 ? w_addr[`MEM_ADDR_WIDTH:2] : 0;
 
 
 	integer i;
@@ -47,10 +49,14 @@ module ram(
 		$readmemb("../../../../init.ram", mem);
 	end
 
-	always @(posedge clk) begin
-		if(we) mem[Ww_index] <= w_data; //書き込みのタイミングを同期
-		addr1_reg <= Wr1_index;           //読み出しアドレスを同期
-		addr2_reg <= Wr2_index;           //読み出しアドレスを同期
+	always @(posedge clk or posedge rst) begin
+		if(rst) begin
+			addr1_reg <= 0; addr2_reg <= 0;
+		end else begin
+			if(we) mem[Ww_index] <= w_data; //書き込みのタイミングを同期
+			addr1_reg <= Wr1_index;           //読み出しアドレスを同期
+			addr2_reg <= Wr2_index;           //読み出しアドレスを同期
+		end
 	end
 
 	assign r1_data = mem[addr1_reg];
