8a5b919d2b4539a7e42db338b39aef7ad75a9165
Turenar <eclair@turenar.xyz>
2017-12-12 11:55:40 +0900

    counter

--- cpu.srcs/sources_1/new/top_module.v
+++ cpu.srcs/sources_1/new/top_module.v
@@ -41,23 +41,24 @@ module top_module(
 		.sysclk(sysclk), .rst(~cpu_resetn),
 		.halt(halt), .instruction_executed(instruction_executed), .errno(errno));
 
-	wire [31:0] counter_data;
-	hardware_counter hc(
+	wire [63:0] hcsc_counter;
+	hardware_counter hc_sysclk(
+		.CLK_IP(sysclk & ~halt), .RSTN_IP(cpu_resetn),
+		.COUNTER_OP(hcsc_counter));
+
+	wire [63:0] hccc_counter;
+	hardware_counter hc_cpuclk(
 		.CLK_IP(instruction_executed), .RSTN_IP(cpu_resetn),
-		.COUNTER_OP(counter_data));
+		.COUNTER_OP(hccc_counter));
 
 	reg [31:0] cpu_inst_executed;
-	reg [2:0] counter;
-	reg dt_we;
-	reg [5:0] dt_waddr;
-	reg [7:0] dt_wdata;
 	wire [7:0] dt_led;
 	display_top dt(
 		.SYSCLK_IP(sysclk), .SW_IP(sw), .CPU_RESETN_IP(cpu_resetn),
 		.LED_OP(dt_led), .OLED_DC_OP(oled_dc), .OLED_RES_OP(oled_res),
 		.OLED_SCLK_OP(oled_sclk), .OLED_SDIN_OP(oled_sdin),
 		.OLED_VBAT_OP(oled_vbat), .OLED_VDD_OP(oled_vdd),
-		.WE_IP(dt_we), .WRITE_ADDR_IP(dt_waddr), .WRITE_DATA_IP(dt_wdata));
+		.WE_IP(ms_we), .WRITE_ADDR_IP(ms_addr), .WRITE_DATA_IP(ms_data));
 
 	assign led[7] = errno != 0;
 	assign led[6:0] = 7'b0 | errno;
@@ -69,14 +70,9 @@ module top_module(
 		end else if (halt) begin
 			if (counter[2] == 0) begin
 				counter <= counter + 1;
-				dt_we <= 1;
-				dt_waddr <= counter;
-				dt_wdata <= counter_data >> (24 - (counter << 3));
 			end else begin
 				dt_we <= 0;
 			end
-		end else if(instruction_executed) begin
-			cpu_inst_executed <= cpu_inst_executed + 1;
 		end
 	end
 endmodule
